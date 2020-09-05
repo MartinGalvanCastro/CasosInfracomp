955a657eb362a0f9fc70e54f4a0947874b9ce062
Martin Galvan <md.galvan@uniandes.edu.co>
2020-09-05 16:04:10 -0500

    Se agrego el config file

--- Caso1/src/clases/Main.java
+++ Caso1/src/clases/Main.java
@@ -1,14 +1,21 @@
 package clases;
-
-
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
+	private static String PATH ="./Caso1/doc/config.txt";
+	
+	
 	public static void main(String[] args) {
 		try {
-			
+			System.out.println("Va a comenzar");
+			Scanner sc = new Scanner(new File(PATH));
+			System.out.println(sc.hasNextLine());
+			while(sc.hasNextLine())
+				System.out.println(sc.nextLine());
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 
 	}
